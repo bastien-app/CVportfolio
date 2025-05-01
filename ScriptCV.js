62f640f2f909b8cd625687010d6e265ade577159
bastien-app <bastien.goumy@outlook.fr>
2025-05-01 02:28:08 +0200

    Debut du menu de navigation

--- ScriptCV.js
+++ ScriptCV.js
@@ -1,19 +1,39 @@
 const nom = "Bastien Goumy";
-        let index = 0;
-        const anim = document.getElementById("nomEcriture");
+let index = 0;
+const anim = document.getElementById("nomEcriture");
         
-        function ecrire() {
-            if (index < nom.length) {
-                anim.textContent += nom.charAt(index);
-                index++;
-                setTimeout(ecrire, 150);
-            } else {
-                setTimeout(clignoterCurseur, 500);
-            }
-        }
+function ecrire() {
+    if (index < nom.length) {
+        anim.textContent += nom.charAt(index);
+        index++;
+        setTimeout(ecrire, 150);
+    } else {
+        setTimeout(clignoterCurseur, 500);
+    }
+}
 
-        function clignoterCurseur() {
-            anim.style.animation = "blink 0.8s step-start infinite"; 
-        }
+function clignoterCurseur() {
+    anim.style.animation = "blink 0.8s step-start infinite"; 
+}
 
-        ecrire();
+ecrire();
+
+const contenu = document.getElementById("contenu");
+const menu = document.getElementById("menu");
+
+
+var observer = new IntersectionObserver(function(afficher) {
+    for (let i = 0; i < afficher.length; i++) {
+        if (afficher[i].isIntersecting) {
+            menu.classList.remove("hidden");
+        } else {
+            menu.classList.add("hidden");
+        }
+    }
+}, {
+    threshold: 0.7
+});
+
+
+
+observer.observe(contenu);
