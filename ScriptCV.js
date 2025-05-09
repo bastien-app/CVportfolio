db2e8374ed4bd9678d4c9de5cea9266a0b6b19b5
bastien-app <bastien.goumy@outlook.fr>
2025-05-10 01:25:16 +0200

    Changement script affichage menu

--- ScriptCV.js
+++ ScriptCV.js
@@ -51,48 +51,76 @@ const sections = document.querySelectorAll("div.texte");
 const animTransi = document.querySelector("div.indicateur");
 const conteneurMenu = document.querySelector(".conteneur-menu");
 
+let currentActiveLink = null;
+
 var observerSection = new IntersectionObserver(function(entries) {
+    let top = window.innerHeight;
+    let targetLink = null;
+
     for (let i = 0; i < entries.length; i++) {
         if (entries[i].isIntersecting) {
-            var id = entries[i].target.id;
-            var lien = document.querySelectorAll('#menu a');
-
-            for (let j = 0; j < lien.length; j++) {
-                lien[j].classList.remove("active");
+            
+           const distanceFromTop = entries[i].boundingClientRect.top;
+            if (distanceFromTop >= 0 && distanceFromTop < top) {
+                top = distanceFromTop;
+                targetLink = entries[i].target;
             }
 
-            
-            var lienActif = document.querySelector('#menu a[href="#' + id + '"]');
-
-            if (lienActif) {
-                lienActif.classList.add("active");
-                
-                /*const posAnimTransi = lienActif.getBoundingClientRect();*/
-                const posLien = lienActif.getBoundingClientRect();
-                const posMenu = conteneurMenu.getBoundingClientRect();
-
-                const relativeTop = posLien.top - posMenu.top;
-                const relativeLeft = posLien.left - posMenu.left;
-                /*console.log(posAnimTransi);*/
-
-                animTransi.style.width = posLien.width + "px";
-                animTransi.style.height = posLien.height + "px";
-
-                animTransi.classList.add("etirement");
-
-                animTransi.style.top = relativeTop + "px";
-                animTransi.style.left = relativeLeft + "px";
-                
-                
-                setTimeout(function(){
-                    animTransi.classList.remove("etirement");
-                }, 300);
-            }
-            
         }
     }
+
+    if (targetLink) {
+
+        var id = targetLink.id;
+
+        if (id === currentActiveLink) {
+            return;
+        }
+
+
+        currentActiveLink = id;
+
+        // On enlève la classe active de tous les liens
+        var lien = document.querySelectorAll('#menu a')
+
+        for (let j = 0; j < lien.length; j++) {
+                lien[j].classList.remove("active");
+        }
+
+
+        var lienActif = document.querySelector('#menu a[href="#' + id + '"]');
+        console.log(lienActif);
+
+        if (lienActif) {
+            
+            lienActif.classList.add("active");
+                
+                
+            const posLien = lienActif.getBoundingClientRect();
+            const posMenu = conteneurMenu.getBoundingClientRect();
+
+            const relativeTop = posLien.top - posMenu.top;
+            const relativeLeft = posLien.left - posMenu.left;
+                
+
+            animTransi.style.width = posLien.width + "px";
+            animTransi.style.height = posLien.height + "px";
+
+            animTransi.classList.add("etirement");
+
+            animTransi.style.top = relativeTop + "px";
+            animTransi.style.left = relativeLeft + "px";
+                
+                
+            setTimeout(function(){
+                animTransi.classList.remove("etirement");
+            }, 300);
+        }
+            
+        
+    }
 }, {
-    threshold: 0.70
+    threshold: [0.1, 0.5, 1.0]
 });
 
 for (let i = 0; i < sections.length; i++) {
