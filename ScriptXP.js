c2941de51377307c9f92cd6cb87482f05cf1768e
bastien-app <bastien.goumy@outlook.fr>
2025-05-17 01:25:15 +0200

    Continuation site xp

--- ScriptXP.js
+++ ScriptXP.js
@@ -4,3 +4,19 @@ window.addEventListener("DOMContentLoaded", () => {
         document.querySelector("main").classList.add("visible");
     }, 4000);
 });
+
+
+/*function openWindow(id) {
+  const windowEl = document.getElementById(id);
+  if (windowEl) {
+    windowEl.hidden = false;
+    windowEl.style.zIndex = 1000; // ou plus si plusieurs fenêtres ouvertes
+  }
+}
+
+function closeWindow(id) {
+  const windowEl = document.getElementById(id);
+  if (windowEl) {
+    windowEl.hidden = true;
+  }
+}*/
