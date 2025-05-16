5ef6610f8d31e4660cd562ff0738c32d3c95542d
Bastien Gmy <bastien.goumy@outlook.fr>
2025-05-16 13:46:57 +0200

    ajout script

---
+++ ScriptXP.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.querySelector(".demarrage").classList.add("hidden");
+        document.querySelector("main").classList.add("visible");
+    }, 4000);
+});
